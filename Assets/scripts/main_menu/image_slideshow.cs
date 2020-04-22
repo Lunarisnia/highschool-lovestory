b1b4d4de6a2448c6c32d569203669cf7936f8f50
loona <r.arswendo.r@gmail.com>
2020-04-22 17:40:03 +0700

    [IMPROVE] added main menu

---
+++ Assets/scripts/main_menu/image_slideshow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class image_slideshow : MonoBehaviour
+{
+    [SerializeField] private float duration = 8f;
+    [SerializeField] private float multiplier = 0.25f;
+    [SerializeField] private Texture[] images;
+    private int selectedImage;
+    private RawImage backgroundImage;
+    void Start()
+    {
+        backgroundImage = GetComponent<RawImage>();
+        selectedImage = Random.Range(0, images.Length);
+        backgroundImage.texture = images[selectedImage];
+
+
+        StartCoroutine(imageSlider(duration, multiplier));
+    }
+
+    IEnumerator imageSlider(float duration, float rawMultiplier)
+    {
+        float multiplier = rawMultiplier * Time.deltaTime;
+        while (true)
+        {
+            selectedImage++;
+            if (selectedImage == images.Length)
+            {
+                selectedImage = 0;
+            }
+            yield return new WaitForSeconds(duration);
+            for (float i = 1; i >= 0; i -= multiplier)
+            {
+                yield return 0;
+                backgroundImage.color = new Color(1, 1, 1, i);
+            }
+            backgroundImage.texture = images[selectedImage];
+            for (float x = 0; x <= 1; x += multiplier)
+            {
+                yield return 0;
+                backgroundImage.color = new Color(1, 1, 1, x);
+            }
+        }
+
+    }
+}
---
+++ Assets/scripts/shared/cooler_highlight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class cooler_highlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] private Text text;
+    [SerializeField] private float pressedAnimationSpeed = 0.5f;
+    private Color defaultColor;
+    void Start()
+    {
+        defaultColor = text.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = Color.red;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = defaultColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        StartCoroutine(pressedCoroutine(pressedAnimationSpeed));
+    }
+    IEnumerator pressedCoroutine(float duration)
+    {
+        text.color = Color.cyan;
+        yield return new WaitForSeconds(duration);
+        text.color = defaultColor;
+    }
+}
